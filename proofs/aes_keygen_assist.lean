22e6138bd3ec282b1d7fd5f5bdc49bd41c0dc7b2
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2026-05-20 14:30:43 +0200

    new aes_keygen and key_expansion

---
+++ proofs/aes_keygen_assist.lean
@@ -0,0 +1,345 @@
+
+-- Experimental lean backend for Hax
+-- The Hax prelude library can be found in hax/proof-libs/lean
+import Hax
+import Std.Tactic.Do
+import Std.Do.Triple
+import Std.Tactic.Do.Syntax
+import sub_bytes
+import key_expansion
+import Utilities
+open Std.Do
+open Std.Tactic
+
+set_option mvcgen.warning false
+set_option linter.unusedVariables false
+
+
+namespace libcrux_aesgcm.platform.portable.aes_core
+
+@[spec]
+def aes_keygen_assisti (rcon : u8) (i : usize) (u : u16) : RustM u16 := do
+  let u3 : u16 ← (u &&&? (61440 : u16));
+  let n : u16 ← (u3 >>>? (12 : i32));
+  let n : u16 ←
+    ((← ((← (n >>>? (1 : i32))) |||? (← (n <<<? (3 : i32))))) &&&? (15 : u16));
+  let ri : u16 ←
+    (rust_primitives.hax.cast_op
+      (← ((← (rcon >>>? i)) &&&? (1 : u8))) :
+      RustM u16);
+  let n : u16 ← (n ^^^? ri);
+  let n : u16 ← (n <<<? (12 : i32));
+  (n ^^^? (← (u3 >>>? (4 : i32))))
+
+def rotate_bt (u : BitVec 4) : BitVec 4 :=
+  let a0 := u.extractLsb 3 3
+  let a1 := u.extractLsb 2 2
+  let a2 := u.extractLsb 1 1
+  let a3 := u.extractLsb 0 0
+  (a3 ++ a0 ++ a1 ++ a2)
+
+def aes_keygen_assisti_spec (rcon : u8) (i : Nat) (u : u16) : BitVec 16 :=
+  let u_bv := u.toBitVec
+  let a0 := u_bv.extractLsb 15 15
+  let a1 := u_bv.extractLsb 14 14
+  let a2 := u_bv.extractLsb 13 13
+  let a3 := u_bv.extractLsb 12 12
+  let top_nib := a0 ++ a1 ++ a2 ++ a3
+  let n_rot := rotate_bt top_nib
+  let ri : BitVec 8 := (rcon.toBitVec >>> i) &&& 1
+  let n := (BitVec.zero 4 ++ n_rot) ^^^ ri
+  (n.extractLsb 3 0) ++ top_nib ++ BitVec.zero 8
+
+
+set_option maxHeartbeats 10000000000000
+set_option maxRecDepth 100000
+@[spec]
+theorem aes_keygen_assisti_correct (rcon : u8) (i : Nat) (u : u16) :
+⦃ ⌜ i < 8 ⌝ ⦄
+  aes_keygen_assisti rcon i.toUSize64 u
+⦃ ⇓ ⟨res⟩ =>
+    ⌜ aes_keygen_assisti_spec rcon i u = res ⌝ ⦄ :=
+  by
+    unfold aes_keygen_assisti
+    hax_mvcgen <;> simp at *
+    .
+      unfold aes_keygen_assisti_spec rotate_bt UInt16.xor
+      rename_auto_n 12
+      simp only [var_11, var_10, var_8, var_7, var_6]
+      simp
+      match i with
+      | n + 8 => grind
+      | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 =>
+          simp
+          bv_decide
+    .
+      grind
+
+@[spec]
+def aes_keygen_assist
+    (next : (RustArray u16 8))
+    (prev : (RustArray u16 8))
+    (rcon : u8) :
+    RustM (RustArray u16 8) := do
+  --Modified by me to get rid of the copy part
+  let next : (RustArray u16 8) ← (aes_core.sub_bytes.sub_bytes_state prev);
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (0 : usize)
+      (← (aes_keygen_assisti rcon (0 : usize) (← next[(0 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (1 : usize)
+      (← (aes_keygen_assisti rcon (1 : usize) (← next[(1 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (2 : usize)
+      (← (aes_keygen_assisti rcon (2 : usize) (← next[(2 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (3 : usize)
+      (← (aes_keygen_assisti rcon (3 : usize) (← next[(3 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (4 : usize)
+      (← (aes_keygen_assisti rcon (4 : usize) (← next[(4 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (5 : usize)
+      (← (aes_keygen_assisti rcon (5 : usize) (← next[(5 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (6 : usize)
+      (← (aes_keygen_assisti rcon (6 : usize) (← next[(6 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (7 : usize)
+      (← (aes_keygen_assisti rcon (7 : usize) (← next[(7 : usize)]_?))));
+  (pure next)
+
+def rotword (v : Vector (BitVec 8) 4) : Vector (BitVec 8) 4 :=
+  #v[v[3], v[0], v[1], v[2]]
+
+def subword (v : Vector (BitVec 8) 4) : Vector (BitVec 8) 4 :=
+  v.map (fun i => aes_core.sub_bytes.get_elem_SBOX (i.extractLsb 7 4) (i.extractLsb 3 0))
+
+def aes_keygen_first (rcon : u8) (chunk : Vector (BitVec 8) 4) : Vector (BitVec 8) 4 :=
+  let new_chunk := (rotword (subword chunk))
+  #v[new_chunk[0], new_chunk[1], new_chunk[2], rcon.toBitVec ^^^ new_chunk[3]]
+
+set_option maxHeartbeats 10000000000000
+set_option maxRecDepth 100000
+@[spec]
+theorem aes_keygen_assist_correct (next : (RustArray u16 8)) (prev : (RustArray u16 8)) (rcon : u8) (a0 a1 a2 a3 : BitVec 8):
+⦃ ⌜
+    true
+⌝ ⦄
+  aes_keygen_assist next prev rcon
+⦃ ⇓ ⟨res⟩ =>
+    ⌜
+      subword (get_word prev.toVec 3) = get_word res 2 /\
+      aes_keygen_first rcon (get_word prev.toVec 3) = get_word res 3
+    ⌝ ⦄ :=
+  by
+    unfold aes_keygen_assist
+    hax_mvcgen[aes_core.sub_bytes.sub_bytes_correct_chunk, aes_keygen_assisti_correct] <;> simp at *
+    .
+      rename_auto_n 70
+      unfold get_word aes_keygen_first subword rotword
+      simp
+      refine ⟨?_, ?_⟩ <;>
+      refine ⟨?_, ?_, ?_, ?_⟩ <;>
+        simp only [var_1] <;>
+        unfold get_elem_bv <;>
+        simp only [Nat.sub_zero, Nat.reduceAdd, BitVec.truncate_eq_setWidth, Nat.reduceLeDiff,
+          Nat.reducePow, Nat.lt_add_one, BitVec.setWidth_ofNat_of_le_of_lt, BitVec.ushiftRight_eq',
+          BitVec.toNat_ofNat, Nat.reduceMod, BitVec.zero_or, ne_eq, reduceCtorEq, not_false_eq_true,
+          Vector.getElem_set_ne, Nat.succ_ne_self, Vector.getElem_set_self, UInt16.toBitVec_xor,
+          UInt16.toBitVec_shiftLeft, UInt16.toBitVec_and, UInt16.toBitVec_or,
+          UInt16.toBitVec_shiftRight, UInt16.toBitVec_ofNat, BitVec.ofNat_eq_ofNat, BitVec.reduceMod,
+          Nat.one_mod, BitVec.reduceShiftRightShiftRight, BitVec.shiftLeft_eq',
+          UInt8.toBitVec_toUInt16, Nat.reduceLT, Nat.reduceEqDiff] <;>
+        simp only [<- var_19, <- var_26, <- var_33, <- var_40, <- var_47, <- var_54, <- var_61, <- var_68] <;>
+        bv_decide
+    all_goals grind
+
+set_option maxHeartbeats 10000000000000
+
+@[spec]
+
+def aes_keygen_assist1.aux (n : u16) : RustM u16 := do
+  let n : u16 ← (n &&&? (3840 : u16));
+  let n : u16 ← (n ^^^? (← (n <<<? (4 : i32))));
+  let n : u16 ← (n ^^^? (← (n >>>? (8 : i32))));
+  (pure n)
+
+@[spec]
+def aes_keygen_assist1 (next : (RustArray u16 8)) (prev : (RustArray u16 8)) :
+    RustM (RustArray u16 8) := do
+  let next : (RustArray u16 8) ← (aes_keygen_assist next prev (0 : u8));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (0 : usize)
+      (← (aes_keygen_assist1.aux (← next[(0 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (1 : usize)
+      (← (aes_keygen_assist1.aux (← next[(1 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (2 : usize)
+      (← (aes_keygen_assist1.aux (← next[(2 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (3 : usize)
+      (← (aes_keygen_assist1.aux (← next[(3 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (4 : usize)
+      (← (aes_keygen_assist1.aux (← next[(4 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (5 : usize)
+      (← (aes_keygen_assist1.aux (← next[(5 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (6 : usize)
+      (← (aes_keygen_assist1.aux (← next[(6 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (7 : usize)
+      (← (aes_keygen_assist1.aux (← next[(7 : usize)]_?))));
+  (pure next)
+
+set_option maxHeartbeats 10000000000000
+set_option maxRecDepth 100000
+@[spec]
+theorem aes_keygen_assist1_correct (next : (RustArray u16 8)) (prev : (RustArray u16 8)) (a0 a1 a2 a3 : BitVec 8):
+⦃ ⌜ true ⌝ ⦄
+  aes_keygen_assist1 next prev
+⦃ ⇓ ⟨res⟩ =>
+    ⌜ let temp := subword (get_word prev.toVec 3)
+      temp = get_word res 3 /\
+      temp = get_word res 2 /\
+      temp = get_word res 1 /\
+      temp = get_word res 0  ⌝ ⦄ :=
+by
+    unfold aes_keygen_assist1
+    hax_mvcgen[aes_keygen_assist_correct] <;> simp at *
+    .
+      rename_auto_n 70
+      simp only [var_1]
+      unfold get_word
+      simp
+      refine ⟨?_, ?_, ?_, ?_⟩ <;>
+      refine ⟨?_, ?_, ?_, ?_⟩ <;>
+      unfold get_elem_bv <;>
+      simp <;>
+      simp only [<- var_19, <- var_22, <- var_25, <- var_28, <- var_31,
+        <- var_34, <- var_37, <- var_40] <;>
+      bv_decide
+    all_goals grind
+
+@[spec]
+def aes_keygen_assist0.aux (n : u16) : RustM u16 := do
+  let n : u16 ← (n &&&? (61440 : u16));
+  let n : u16 ← (n ^^^? (← (n >>>? (4 : i32))));
+  let n : u16 ← (n ^^^? (← (n >>>? (8 : i32))));
+  (pure n)
+
+@[spec]
+def aes_keygen_assist0
+    (next : (RustArray u16 8))
+    (prev : (RustArray u16 8))
+    (rcon : u8) :
+    RustM (RustArray u16 8) := do
+  let next : (RustArray u16 8) ← (aes_keygen_assist next prev rcon);
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (0 : usize)
+      (← (aes_keygen_assist0.aux (← next[(0 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (1 : usize)
+      (← (aes_keygen_assist0.aux (← next[(1 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (2 : usize)
+      (← (aes_keygen_assist0.aux (← next[(2 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (3 : usize)
+      (← (aes_keygen_assist0.aux (← next[(3 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (4 : usize)
+      (← (aes_keygen_assist0.aux (← next[(4 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (5 : usize)
+      (← (aes_keygen_assist0.aux (← next[(5 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (6 : usize)
+      (← (aes_keygen_assist0.aux (← next[(6 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (7 : usize)
+      (← (aes_keygen_assist0.aux (← next[(7 : usize)]_?))));
+  (pure next)
+
+set_option maxHeartbeats 10000000000000
+set_option maxRecDepth 100000
+@[spec]
+theorem aes_keygen_assist0_correct (rcon : u8) (next : (RustArray u16 8)) (prev : (RustArray u16 8)) (a0 a1 a2 a3 : BitVec 8):
+⦃ ⌜ true ⌝ ⦄
+  aes_keygen_assist0 next prev rcon
+⦃ ⇓ ⟨res⟩ =>
+    ⌜ let temp := aes_keygen_first rcon (get_word prev.toVec 3)
+      temp = get_word res 3 /\
+      temp = get_word res 2 /\
+      temp = get_word res 1 /\
+      temp = get_word res 0  ⌝ ⦄ :=
+by
+    unfold aes_keygen_assist0
+    hax_mvcgen[aes_keygen_assist_correct] <;> simp at *
+    .
+      rename_auto_n 70
+      simp only [var_1]
+      unfold get_word
+      simp
+      refine ⟨?_, ?_, ?_, ?_⟩ <;>
+      refine ⟨?_, ?_, ?_, ?_⟩ <;>
+      unfold get_elem_bv <;>
+      simp <;>
+      simp only [<- var_19, <- var_22, <- var_25, <- var_28, <- var_31,
+        <- var_34, <- var_37, <- var_40] <;>
+      bv_decide
+    all_goals grind
+
+
+end libcrux_aesgcm.platform.portable.aes_core
---
+++ proofs/key_expansion.lean
@@ -0,0 +1,140 @@
+
+-- Experimental lean backend for Hax
+-- The Hax prelude library can be found in hax/proof-libs/lean
+import Hax
+import Std.Tactic.Do
+import Std.Do.Triple
+import Std.Tactic.Do.Syntax
+import Utilities
+open Std.Do
+open Std.Tactic
+
+set_option mvcgen.warning false
+set_option linter.unusedVariables false
+
+
+namespace libcrux_aesgcm.platform.portable.aes_core
+
+@[spec]
+def key_expand1 (p : u16) (n : u16) : RustM u16 := do
+  let p : u16 ←
+    ((← ((← (p ^^^? (← ((← (p &&&? (4095 : u16))) <<<? (4 : i32)))))
+        ^^^? (← ((← (p &&&? (255 : u16))) <<<? (8 : i32)))))
+      ^^^? (← ((← (p &&&? (15 : u16))) <<<? (12 : i32))));
+  (n ^^^? p)
+
+def key_expand1_spec (p : u16) (n : u16) : BitVec 16 :=
+  let p_bv := p.toBitVec
+  let a := p_bv.extractLsb 15 12
+  let b := p_bv.extractLsb 11 8
+  let c := p_bv.extractLsb 7 4
+  let d := p_bv.extractLsb 3 0
+
+  n.toBitVec ^^^ ((a ^^^ b ^^^ c ^^^d) ++ (b ^^^ c ^^^ d) ++ (c ^^^ d) ++ d)
+
+set_option maxHeartbeats 10000000000000
+set_option maxRecDepth 100000
+theorem key_expand1_correct (p : u16) (n : u16) :
+⦃ ⌜ true ⌝ ⦄
+  key_expand1 p n
+⦃ ⇓ ⟨res⟩ =>
+    ⌜ key_expand1_spec p n = res ⌝ ⦄ :=
+  by
+    unfold key_expand1
+    hax_mvcgen <;> simp at *
+    unfold key_expand1_spec
+    simp
+    rename_auto_n 7
+    simp only [var_4, var_5, var_6, UInt16.xor]
+    bv_decide
+
+
+@[spec]
+def key_expansion_step (next : (RustArray u16 8)) (prev : (RustArray u16 8)) :
+    RustM (RustArray u16 8) := do
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (0 : usize)
+      (← (key_expand1 (← prev[(0 : usize)]_?) (← next[(0 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (1 : usize)
+      (← (key_expand1 (← prev[(1 : usize)]_?) (← next[(1 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (2 : usize)
+      (← (key_expand1 (← prev[(2 : usize)]_?) (← next[(2 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (3 : usize)
+      (← (key_expand1 (← prev[(3 : usize)]_?) (← next[(3 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (4 : usize)
+      (← (key_expand1 (← prev[(4 : usize)]_?) (← next[(4 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (5 : usize)
+      (← (key_expand1 (← prev[(5 : usize)]_?) (← next[(5 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (6 : usize)
+      (← (key_expand1 (← prev[(6 : usize)]_?) (← next[(6 : usize)]_?))));
+  let next : (RustArray u16 8) ←
+    (rust_primitives.hax.monomorphized_update_at.update_at_usize
+      next
+      (7 : usize)
+      (← (key_expand1 (← prev[(7 : usize)]_?) (← next[(7 : usize)]_?))));
+  (pure next)
+
+def xor_word (a b : Vector (BitVec 8) 4) : Vector (BitVec 8) 4 :=
+  Vector.map
+    (fun (p : BitVec 8 × BitVec 8) => p.1 ^^^ p.2) (Vector.zip a b)
+
+set_option maxHeartbeats 10000000000000
+set_option maxRecDepth 100000
+theorem key_expansion_correct2 (next : (RustArray u16 8)) (prev : (RustArray u16 8)) :
+⦃ ⌜ true ⌝ ⦄
+  key_expansion_step next prev
+⦃ ⇓ ⟨res⟩ =>
+    ⌜
+    let row_prev i := get_word prev.toVec (3 - i)
+    let row_next i := get_word next.toVec (3 - i)
+    let row_res i := get_word res (3 - i)
+    row_res 3 = xor_word (row_next 3) (row_prev 3) /\
+    row_res 2 = xor_word (row_next 2) (xor_word (row_prev 2) (row_prev 3)) /\
+    row_res 1 = xor_word (row_next 1) (xor_word (row_prev 1) (xor_word (row_prev 2) (row_prev 3))) /\
+    row_res 0 = xor_word (row_next 0) (xor_word (row_prev 0) (xor_word (row_prev 1) (xor_word (row_prev 2) (row_prev 3))))
+    ⌝ ⦄ :=
+  by
+    unfold key_expansion_step
+    hax_mvcgen[key_expand1_correct] <;> simp at *
+    unfold get_word get_elem_bv xor_word
+    .
+      intros
+      simp only [Nat.sub_zero, Nat.reduceAdd, ne_eq, reduceCtorEq, not_false_eq_true,
+        Vector.getElem_set_ne, Nat.succ_ne_self, Vector.getElem_set_self, BitVec.ofNat_eq_ofNat,
+        BitVec.mul_zero, BitVec.zero_add, BitVec.truncate_eq_setWidth, Nat.reduceLeDiff,
+        Nat.reducePow, Nat.reduceLT, BitVec.setWidth_ofNat_of_le_of_lt, BitVec.ushiftRight_eq',
+        BitVec.toNat_ofNat, Nat.reduceMod, BitVec.zero_or, Nat.reduceEqDiff, Nat.one_mod,
+        BitVec.add_zero, Nat.zero_lt_succ, Nat.zero_mod, BitVec.ushiftRight_zero, Vector.mk_zip_mk,
+        List.zip_toArray, List.zip_cons_cons, List.zip_nil_right, Vector.map_mk, List.map_toArray,
+        List.map_cons, List.map_nil, Vector.eq_mk, Array.mk.injEq, List.cons.injEq, and_true,
+        BitVec.mul_one, BitVec.reduceAdd, BitVec.reduceMul, Nat.lt_add_one]
+      rename_auto_n 67
+      simp only [<- var_3, <- var_9, <- var_15, <- var_21, <- var_27, <- var_33, <- var_39, <- var_45]
+      unfold key_expand1_spec
+      simp only [Nat.reduceSub, Nat.reduceAdd, Nat.sub_zero]
+      simp only [ <- var_49, <- var_50, <- var_51, <- var_52, <- var_53, <- var_54, <- var_55, <- var_56]
+      simp only [<- var_6, <- var_12, <- var_18, <- var_24, <- var_30, <- var_36, <- var_42, <- var_48]
+      bv_decide
+    all_goals grind
+
+end libcrux_aesgcm.platform.portable.aes_core
